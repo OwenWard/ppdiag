25cc4084a5a2bd71309f79318de77cfc48afbcf4
Xiaoxi Zhao <xz2740@columbia.edu>
2019-04-28 13:54:57 -0400

    package

---
+++ snadv/R/snadv-package.R
@@ -0,0 +1,8 @@
+#' snadv: Perform diagnosis and visualizations for social network analysis
+#'
+#' The model checking process is an important part in the social network analysis.
+#' This package includes multiple methods to plot the model as well as the true data, to compare the model fit and model performace.
+#'
+#' @name snadv
+#' @docType package
+NULL
