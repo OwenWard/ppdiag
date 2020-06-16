8eaff6c76cadb04a90e1bfd66591cdba81e02d8b
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-06-16 23:06:30 +0800

    hpp.R: simulate homogeneous poisson process events
    input lambda, n, t0, output vector of length n

---
+++ R/hpp.R
@@ -0,0 +1,12 @@
+#' Simulate homogeneous poisson process events
+#'
+#' @param lambda rate of the poisson process
+#' @param n number of events
+#' @param t0 start time of events
+#' @importFrom 
+#' @return a vector of length n
+#' @export
+
+hpp <- function(lambda,n,t0=0){
+	return(t0+cumsum(rexp(n=n,rate=lambda)))
+}
