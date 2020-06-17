433ef9e58390c98a4b6adc59cbcb587161048c92
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-06-17 18:33:43 +0800

    fit a homogeneous poisson process model
    return a hpp object

---
+++ R/hpp.R
@@ -0,0 +1,16 @@
+#' Create a homogeneous poisson process model
+#'
+#' Create a homogeneous poisson process according to given parameters: lambda, start, end, and n.
+#' @param lambda rate of the poisson process
+#' @param n number of events
+#' @param start start time of events
+#' @param end end time of events
+#' 
+#' @return hpp object
+#' @export
+
+hp <- function(lambda, start=0, end=0, n = NULL) {
+  y <- c(list(lambda=lambda, start=start, end=end, n=n))
+  class(y) <- "hpp"
+  return(y)
+}
