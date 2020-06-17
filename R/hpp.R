c2643892abf84fcfd7945cb5318e911fd1648d46
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-06-17 16:12:45 +0800

    adds parameter to simulate hpp
    If user enters both end and n, we generate n number of events so resulting end time might be different from what the user enters, so we print a message telling them if they care about end time, they should respecify end and leave n blank.  
    
    If user only enters end, we generate events starting from start and ending with end.

--- R/hpp.R
+++ R/hpp.R
@@ -2,10 +2,23 @@
 #'
 #' @param lambda rate of the poisson process
 #' @param n number of events
-#' @param t0 start time of events
+#' @param start start time of events
+#' @param end end time of events
+#' 
 #' @return a vector of length n
 #' @export
 
-hpp <- function(lambda,n,t0=0){
-	return(t0+cumsum(rexp(n=n,rate=lambda)))
+hpp <- function(lambda,start=0,end=0,n=NULL){
+  if(!is.null(n)){
+    if(end>start){
+      cat("hpp events with length n will be generated, end time might be 
+          different from the value you entered, if you have a preferred 
+          end time, please rerun the function without specifying n.")
+    }
+    return(cumsum(c(start,-log(runif(n-1))/lambda)))
+  }else{
+    n=rpois(n=1,lambda=lambda*end)
+    u=(end-start)*runif(n-1)+start
+    return (sort(c(start,u)))
+  }
 }
