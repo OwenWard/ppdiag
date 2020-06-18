b9ca9bbec22a790f085bcef687f36dbdbff7974c
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-06-18 19:07:03 +0800

    plot hpp
    This plots events counts for hpp.

---
+++ R/plothpp.R
@@ -0,0 +1,16 @@
+#' Draw Homogeneous Poisson Process 
+#'
+#' Draw the event time counts for homogeneous poisson process
+#'
+#' @param object parameters for homogeneous poisson process
+#' @param color A specification for the default plotting color.
+#' @importFrom graphics plot
+
+#' @export
+
+
+plothpp <- function(events,color){
+	count=0:length(events)
+	plot(stepfun(x=events, y=count), main="Homogeneous Poisson Process",
+       xlab="Event Time", ylab="Number of events", col=color)
+}
