e5332662ffd7921e69cfe1a3339107a0a2cfb043
Qian Sun <qiansun@brandeis.edu>
2020-06-24 15:31:07 +0800

    add hpp event points to drawHPPIntensity

--- R/drawHPPIntensity.R
+++ R/drawHPPIntensity.R
@@ -6,7 +6,8 @@
 #' @param color a specification for the default plotting color.
 #' @importFrom graphics plot
 #' @importFrom graphics abline
-
+#' @importFrom graphics points
+#' @importFrom graphics points
 #' @export
 
 
@@ -18,8 +19,14 @@ drawHPPIntensity <- function(hpp,color){
 	if(!is.null(n)){
 		events=simulatehpp(lambda=lambda, start=start, end=end, n=n) #if n not null, we need to simulate to get end
 		end=max(events)
+	}else{
+	  events=simulatehpp(lambda=lambda, start=start, end=end)
 	}
 	plot(c(start,end), c(0,lambda*2), type = "n", xlab = "event times", ylab = "lambda", 
 		main="Intensity of homogeneous poisson process")
 	abline(h=lambda, col=color)
+	for(i in 1:length(events)){
+	  points(x=events[i],y=0,pch=1,col="blue")
+	}
+	legend("topleft", "Homogeneous Poisson Process events", col = "blue", pch = 1)
 }
