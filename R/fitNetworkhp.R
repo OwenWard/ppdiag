9699d9ee41a785c7533197ecc62fef938b547af1
delduca-xlh <52299750+delduca-xlh@users.noreply.github.com>
2020-06-29 16:08:43 +0800

    fit hp to each given pair of events in the network

---
+++ R/fitNetworkhp.R
@@ -0,0 +1,24 @@
+#' Fit multiple hawkes processes to a network using negloglik and optim functions
+#'
+#' Compute the maximum likelihood parameter values for hawkes process.
+#' 
+#' @param vec vector containing initial values for the object parameters (lambda0,alpha,beta) to be optimized.
+#' @param t network describing in a datatable containing the sender, receiver and event times.
+#' @param termination the end time of event times.
+#' @importFrom stats optim
+#' @return a list of hp object indicating the maximum likelihood parameter values (lambda0,alpha,beta) for hawkes process plus the sender and receiver information.
+#' @export
+
+
+
+fit.network.hp <- function(vec, t, termination){
+  t  = t%>%group_by(Send,Rec)%>% summarise(Times =list(Times))
+  network_hp_object = vector(mode = "list")
+  for (i in 1:nrow(t)){
+    hp_object = fithp(vec,t[i,'Times'][[1]],termination)
+    network_hp_object = c(network_hp_object,list(Send=t[i,'Send'][[1]], Rec=t[i,'Rec'][[1]], Para=hp_object)) 
+  }
+  class(network_hp_object) = "network_hp"
+  return (network_hp_object)
+}
+
