271585c3e87f0033b94d7304d4b23d76bffb6aba
delduca-xlh <52299750+delduca-xlh@users.noreply.github.com>
2020-06-30 14:13:24 +0800

    updates to data frame output format

--- R/fitNetworkhp.R
+++ R/fitNetworkhp.R
@@ -13,10 +13,14 @@
 
 fit.network.hp <- function(vec, t, termination){
   t  = t%>%group_by(Send,Rec)%>% summarise(Times =list(Times))
-  network_hp_object = vector(mode = "list")
+  network_hp_object <- data.frame(matrix(ncol = 3, nrow = nrow(t)))
+  x <- c("Send", "Rec","hp_parameters")
+  colnames(network_hp_object) <- x
   for (i in 1:nrow(t)){
     hp_object = fithp(vec,t[i,'Times'][[1]],termination)
-    network_hp_object = c(network_hp_object,list(Send=t[i,'Send'][[1]], Rec=t[i,'Rec'][[1]], Para=hp_object)) 
+    network_hp_object[i , "Send"] <- t[i,'Send'][[1]]
+    network_hp_object[i , "Rec"] <- t[i,'Rec'][[1]]
+    network_hp_object[i , "hp_parameters"][[1]] <- list(hp_object)
   }
   class(network_hp_object) = "network_hp"
   return (network_hp_object)
