5c2b412958e08baa330912edc16d67c84c50f865
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-07-08 18:19:54 +0800

    add example and stop message for hp.R

--- R/hp.R
+++ R/hp.R
@@ -10,8 +10,12 @@
 #'
 #' @return hp object
 #' @export
-
+#' @examples
+#' hp(lambda0 = 0.1,alpha = 0.45,beta = 0.5)
 hp <- function(lambda0, alpha, beta, tau = NULL) {
+  if(alpha>=beta){
+    stop("A stationary hawkes process requires alpha<beta.")
+  }
   y <- c(list(lambda0 = lambda0, alpha = alpha, beta = beta, tau = tau))
   class(y) <- "hp"
   return(y)
