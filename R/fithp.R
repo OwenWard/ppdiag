e3d3fa3cc65db1ce77ed9f516317dcf2e9f42e4a
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-07-14 17:33:11 +0800

    Update fithp.R

--- R/fithp.R
+++ R/fithp.R
@@ -4,7 +4,7 @@
 #' 
 #' @param vec vector containing initial values for the object parameters (lambda0,alpha,beta) to be optimized.
 #' @param t vector containing event times.
-#' @param termination the end time of event times.
+#' @param end the end time of event times.
 #' @importFrom stats optim
 #' 
 #' @return a hp object indicating the maximum likelihood parameter values (lambda0,alpha,beta) for hawkes process.
@@ -14,17 +14,17 @@
 #' sims <- simulatehp(hp_obj,start = 0, end = 100, history = 0)
 #' fithp(init,sims$t,max(sims$t))
 
-negloglik_hp<-function(vec,t,termination){
+negloglik_hp<-function(vec,t,end){
 	#transforms input list object into vector so that it can be used in optim 
 	object=list(lambda0=vec[1], alpha=vec[2], beta=vec[3])
 	class(object) = "hp"
-    negloglik(object=object, t=t, termination=termination)
+    negloglik(object=object, t=t, termination=end)
 }
 
                   
-fithp<-function(vec,t,termination){
+fithp<-function(vec,t,end){
 	hawkes.par=optim(par=vec, fn=negloglik_hp, 
-                    t=t, termination=termination, control = list(maxit = 1000),lower = c(1e-4,1e-4,1e-4),
+                    t=t, termination=end, control = list(maxit = 1000),lower = c(1e-4,1e-4,1e-4),
                   method = "L-BFGS-B")
     hp_object = list(lambda0=hawkes.par$par[1], alpha=hawkes.par$par[2], beta=hawkes.par$par[3])
     class(hp_object) = "hp"
