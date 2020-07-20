de2038b28d423f4f20725ff620d56f9858d8bcd9
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2020-07-20 18:36:57 -0400

    make qqplot square

--- R/qqexp.R
+++ R/qqexp.R
@@ -16,9 +16,12 @@
 qqexp <- function(r, ...) {
   p <- ppoints(100) # 100 equally spaced points on (0,1), excluding endpoints
   q <- quantile(r, p = p) # percentiles of the sample distribution
+  max_range <- max(c(max(qexp(p)),q))
   plot(qexp(p), q,
     main = "Exponential Q-Q Plot",
-    xlab = "Theoretical Quantiles", ylab = "Sample Quantiles", ...
+    xlab = "Theoretical Quantiles", ylab = "Sample Quantiles", 
+    xlim = c(0,max_range),
+    ylim = c(0,max_range), ...
   )
   qqline(q, distribution = qexp, col = "blue", lty = 2)
 }
