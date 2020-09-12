442cdaf7d5da4fb63b6ca5cac2d05c538a0ce006
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-09-12 19:51:11 +0800

    typo

--- tests/testthat/simulate-hp.R
+++ tests/testthat/simulate-hp.R
@@ -1,7 +1,7 @@
 test_that("simulate hp", {
   sim=simulatehp(hp=hp(1.5,0.1,0.2),end=10)
   expect_type(sim, "list")
-  expect_equal(length(hp), 2)
+  expect_equal(length(sim), 2)
   expect_true(!is.null(sim$t))
   
 })
