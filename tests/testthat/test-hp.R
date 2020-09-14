c382061bf7e72084e1042786c33df91b576d811d
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-09-14 19:14:11 +0800

    test-hp now includes testing for simulating and fitting hp

--- tests/testthat/test-hp.R
+++ tests/testthat/test-hp.R
@@ -1,5 +1,11 @@
-test_that("fit hp", {
+test_that("test hp", {
+  #test simulate hp
   sim=simulatehp(hp=hp(1.5,0.1,0.2),end=10)
+  expect_type(sim, "list")
+  expect_equal(length(sim), 2)
+  expect_true(!is.null(sim$t))
+  
+  #test fit hp
   events=sim$t
   hp=fithp(vec=rep(0.1,3),t=events, end=10)
   expect_type(hp, "list")
