193967d7bd794afb71402744dcf14d3d932a554f
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-09-14 19:19:13 +0800

    test-hpp now includes test on simulating and fitting hpp

--- tests/testthat/test-simulate-hpp.R
+++ tests/testthat/test-simulate-hpp.R
@@ -1,6 +1,12 @@
-test_that("simulate hpp", {
+test_that("test hpp", {
+  #test fit hpp
   hpp_obj=hpp(lambda = 1, end = 10, n=50)
   sim=simulatehpp(hpp_obj)
+  hpp=fithpp(sim)
+  expect_type(hpp, "list")
+  expect_length(hpp, 5)
+  
+  #test simulate hpp
   expect_type(sim, "double")
   expect_true(!is.null(sim))
   
