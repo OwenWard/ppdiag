65d0f1548745bc0a548c2264e10061f5ac12d66f
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-09-21 00:06:48 +0800

    added error message testing in test-mmhp

--- tests/testthat/test-mmhp.R
+++ tests/testthat/test-mmhp.R
@@ -3,4 +3,7 @@ test_that("multiplication works", {
   x <- mmhp(Q, delta = c(1 / 3, 2 / 3), lambda0 = 0.9, lambda1 = 1.1, alpha = 0.8, beta = 1.2)
   expect_type(x, "list")
   expect_gte(length(x), 7)
+  
+  #test for error messages
+  expect_error(simulatemmhp(mmhp(lambda0 = 0.9, lambda1 = 1.1, alpha=1,beta=0.5)),"Require alpha less than beta for a stationary process")
 })
