e8a74a580c2ed89bf0f1db24e313758f82e9d101
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-09-21 22:46:37 +0800

    add edge case tests in test-hpp

--- tests/testthat/test-hpp.R
+++ tests/testthat/test-hpp.R
@@ -10,6 +10,18 @@ test_that("test hpp", {
   expect_type(sim, "double")
   expect_true(!is.null(sim))
   
+  #test for edge cases
+  #start>end
+  hpp_obj=hpp(lambda = 1, start=20, end = 10)
+  sim=simulatehpp(hpp_obj)
+  expect_type(sim, "double")
+  expect_true(!is.null(sim))
+  
+  #n=0
+  hpp_obj=hpp(lambda = 1, start=20, end = 10,n=0)
+  sim=simulatehpp(hpp_obj)
+  expect_null(sim)
+  
   #test for error messages
   expect_error(simulatehpp(hpp(lambda=1,start=2,end=2)),"Start and end time identical")
   expect_message(simulatehpp(hpp(lambda=1,start=2,end=3,n=10)),"10 events simulated, end time specified will be ignored. To simulate events up to an endtime do not specify n.")
