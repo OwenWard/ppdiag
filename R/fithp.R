f7924c01d7a07bb878602a66c1ff393718db63ee
sallyqiansun <44779808+sallyqiansun@users.noreply.github.com>
2020-09-27 12:54:35 +0800

    shorten error message in fithp

--- R/fithp.R
+++ R/fithp.R
@@ -38,7 +38,7 @@ fithp<-function(vec,t,end){
     a=hawkes.par$par[2]
     b=hawkes.par$par[3]
     while(TRUE){
-	  message("A stationary hawkes process requires alpha<beta. Refitting events to get a stationary hawkes process object.")
+	  message("A stationary hawkes process requires alpha<beta. Now refitting.")
 	  hawkes.par=optim(par=vec, fn=negloglik_hp, 
 	                   t=t, end=end, control = list(maxit = 1000),lower = c(1e-4,1e-4,1e-4),
 	                   method = "L-BFGS-B")
