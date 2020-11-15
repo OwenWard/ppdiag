06edf5df5f97dd276b6ddb65c45c3518fc4a2347
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2020-11-15 16:31:51 -0500

    make the intensity calculation even simpler here

--- R/rawresidual.R
+++ R/rawresidual.R
@@ -65,9 +65,7 @@ rawresidual.mmhp <- function(object, events, start = 0,
 rawresidual.hpp <- function(object, events, start = 0, 
                             end = max(events)) {
   N <- length(events)
-  event_obj <- list(events = events, start = start, end = end)
-  est.intensity <- intensity(object, event = event_obj,
-                             method = "integral")
-  all_Lambda <- sum(est.intensity)
+  
+  all_Lambda <- object$lambda*(end - start)
   return(N - all_Lambda)
 }
