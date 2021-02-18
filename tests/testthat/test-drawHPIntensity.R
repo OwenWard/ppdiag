599bffcbcdf58974c645738240cbd2b812ac3431
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2021-02-18 08:50:43 -0500

    correct test

--- tests/testthat/test-drawHPIntensity.R
+++ tests/testthat/test-drawHPIntensity.R
@@ -3,11 +3,11 @@ test_that("test drawHPIntensity", {
   hp_obj <- pp_hp(lambda0 = 0.1, alpha = 0.45, beta = 0.5)
   expect_error(drawHPIntensity(hp_obj, events = NULL),"Events must be provided either in the object or in the events argument. ")
   
-  sims <- pp_simulate(hp_obj, start = 0, end = 20)
+  sims <- pp_simulate(hp_obj, n = 20 )
   events <- sims
   expect_message(drawHPIntensity(hp_obj, events = events),"Using the hp object. Set fit = TRUE to fit events provided.")
   
-  sims <- pp_simulate(hp_obj, start = 0, n = 10)
+  sims <- pp_simulate(hp_obj, n = 10)
   events <- sims
   expect_message(drawHPIntensity(hp_obj, events = events, fit = TRUE),"Fitting provided events.")
   
