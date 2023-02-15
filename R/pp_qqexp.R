43fa348b4ca169060176307d70d0fa5ed90c7e5e
OwenWard <owengward@gmail.com>
2023-02-15 15:41:43 -0800

    remove the axis limits for qq plot

--- R/pp_qqexp.R
+++ R/pp_qqexp.R
@@ -30,12 +30,12 @@ pp_qqexp <- function(r, ...) {
   }
   p <- ppoints(100) # 100 equally spaced points on (0,1), excluding endpoints
   q <- quantile(r, p = p) # percentiles of the sample distribution
-  max_range <- max(c(max(qexp(p)), q))
+  # max_range <- max(c(max(qexp(p)), q))
   plot(qexp(p), q,
     main = "Q-Q Plot",
-    xlab = "Theoretical Quantiles", ylab = "Sample Quantiles",
-    xlim = c(0, max_range),
-    ylim = c(0, max_range), ...
+    xlab = "Theoretical Quantiles", ylab = "Sample Quantiles", ...
+    # xlim = c(0, max_range),
+    # ylim = c(0, max_range), ...
   )
   qqline(q, distribution = qexp, col = "blue", lty = 2)
 }
